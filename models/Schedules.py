f9876b581568fa0ca2fa86a195696d98f36f9a0a
MarceloRother <marcelorotherwork@gmail.com>
2025-02-24 20:30:03 -0300

    Organiza Cadastre

--- models/Schedules.py
+++ models/Schedules.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 class Schedules:
-    def __init__(self, time_in, hourly_load, lunch_time, initial_vacation, finish_vacation):
+    def __init__(self, time_in, hourly_load, lunch_time):
         self.time_in: datetime = time_in
         self.hourly_load: datetime = hourly_load
         self.lunch_time: datetime = lunch_time
-        self.initial_vacation: datetime = initial_vacation
-        self.finish_vacation: datetime = finish_vacation
+        self.initial_vacation: datetime = datetime()
+        self.finish_vacation: datetime = datetime()
