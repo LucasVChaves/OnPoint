82a14f0c7abd6c70dddc8be6da65d71686f9f31a
Lucas <lucasvchaves@gmail.com>
2025-02-24 23:58:49 -0300

    feat: Adiciona prototipos dos metodos proprios do admin

--- models/Admin.py
+++ models/Admin.py
@@ -7,5 +7,20 @@ class Admin(Employee):
         self.cadastre: Cadastre = Cadastre()
         self.json_manager: JSONManager = JSONManager()
 
+    def get_employee(self, id: str):
+        return self.json_manager.load_from_json('employee', id)
+
     def set_employee(self, employee):
         self.json_manager.save_to_json('employee', employee.get_id(), employee)
+
+    # frequencia em % (100% do mes = 22 dias uteis)
+    def fix_monthly_frequency(self, id: str, new_frequency: float):
+        pass
+
+    # vai iterar pelo clocks.json somando todas
+    # as horas trabalhadas no mes corrente
+    def generate_monthly_worked_hours_report(self, id: str):
+        pass
+
+    def sync_holydays_callendar(self):
+        pass
