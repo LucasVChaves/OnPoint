a54fa9001061b851dfde07d89e8c642a389ce90c
MarceloRother <marcelorotherwork@gmail.com>
2025-02-26 04:02:04 -0300

    Login funcionando

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 from tkinter import *
-from views import GUI
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))  # Adiciona dinamicamente
+from views.GUI import GUI
 # Esse trem esquisito serve pra garantir esse
 # arquivo como entrypoint
 if __name__ == "__main__":
-    app = GUI  # Inicia a interface gráfica
+    app = GUI()  # Inicia a interface gráfica
+    app.login_label()  # Executa a interface gráfica
